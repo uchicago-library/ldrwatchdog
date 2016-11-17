578bcc8d51c5623d85dac316ad5d92877089b196
Tyler Danstrom <tdanstrom@uchicago.edu>
2016-11-17 13:26:48 -0600

    reworked cached_data so it caches the whole accession not individual files. checks if accession is done before adding a file to the list of premis records to copy over

--- ldrwatchdog/app/postarchiver.py
+++ ldrwatchdog/app/postarchiver.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 from os import _exit, makedirs, scandir
-from os.path import dirname, exists, isdir, join
+from os.path import dirname, exists, isdir, join, relpath
 import json 
 
 from uchicagoldrtoolsuite.bit_level.lib.ldritems.ldrpath import LDRPath
@@ -11,8 +11,18 @@ __EMAIL__ = "tdanstrom@uchicago.edu"
 __VERSION__ = "1.0.0"
 __DESCRIPTION__ = "a command line tool to find all premis records in longTermStorage and if not already in livePremis copy the file into livePremis"
 
+def retrieve_accession_from_path(a_path, lts_root):
+    """a function to extract the accession id from the path to a file from longTermStorage
 
-def scantree(path, cache_file=None):
+    __Args__
+    1. a_path (str): a longTermStorage path
+
+    """
+    path_str = relpath(a_path.path, lts_root)
+    accession = path_str[0:path_str.index("arf")].replace('/','')
+    return accession
+
+def scantree(path, cache_file=None, lts_root=None):
     """a function to take a path and scan it recursively; building up a generator of premis records
 
     __Args__
@@ -20,8 +30,8 @@ def scantree(path, cache_file=None):
     """
     for entry in scandir(path):
         if entry.is_dir():
-            yield from scantree(entry.path, cache_file=cache_file)
-        elif 'premis.xml' in entry.path and cache_file.get(entry.path) == None:
+            yield from scantree(entry.path, cache_file=cache_file, lts_root=lts_root)
+        elif 'premis.xml' in entry.path and cache_file.get(retrieve_accession_from_path(entry, lts_root)) == None:
             yield entry
 
 def main(cached_file, longterm, live_premis):
@@ -35,18 +45,19 @@ def main(cached_file, longterm, live_premis):
         cache_data = json.load(json_data)
     new_cache_data = cache_data
     try:
-        longterm_accessions = scantree(longterm, cache_file=cache_data)
-        for n in longterm_accessions:
-            live_record_path = join(live_premis, n.path.split('pairtree_root')[1])
-            relative_path = n.path.split("/data/repository/longTermStorage")[1]
-            source_string = n.path
-            destination_string = live_premis + relative_path
+        longterm_accessions = scantree(longterm, cache_file=cache_data, lts_root=longterm)
+        for n_entry in longterm_accessions:
+            live_record_path = join(live_premis, n_entry.path.split('pairtree_root')[1])
+            relative_path = relpath(n_entry.path, longterm)
+            source_string = n_entry.path
+            destination_string = join(live_premis, relative_path)
             makedirs(dirname(destination_string), exist_ok=True)
             source_path = LDRPath(source_string)
             destination_path = LDRPath(destination_string)
             copier = LDRItemCopier(source_path, destination_path, eq_detect="name")
             copier.copy()
-            new_cache_data[source_string] = True
+            new_cache_data[retrieve_accession_from_path(n_entry, longterm)] = True
+            print(new_cache_data)
         with open(cached_file, 'w') as write_file:
             json.dump(new_cache_data, write_file)
         return 0
