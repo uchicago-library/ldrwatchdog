23290de73d2e7b50b3c66a585eea5570efbea743
Tyler Danstrom <tdanstrom@uchicago.edu>
2016-11-17 10:07:59 -0600

    wrote setup.py and added README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# About the Watchdog
+
+This is a set of tools that are intended to run as cronjobs on a server hosting a digital repository conforming to the stickshift repository system. 
+
+It uses the livePremis environment to do the following
+
+1. perform regular fixity checks
+2. add new premis records to the livePremis environment when new material is added to longTermStorage
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+setup(
+    name = 'ldrwatchdog',
+    description = "A suite of tools for performing watchdog functionality on digital repositories",
+    long_description = readme(),
+    version = '0.0.1dev',
+    author = "Brian Balsamo, Tyler Danstrom",
+    author_email = "balsamo@uchicago.edu, tdanstrom@uchicago.edu",
+    keywords = [
+        "uchicago",
+        "repository",
+        "file-level",
+        "watchdog"
+    ],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Operating System :: Unix",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    dependency_links = [
+        'https://github.com/uchicago-library/uchicagoldr-toolsuite' +
+        '/tarball/master#egg=uchicagoldrtoolsuite',
+        'https://github.com/uchicago-library/uchicagoldr-premiswork' +
+        '/tarball/master#egg=pypremis',
+    ]
+)
